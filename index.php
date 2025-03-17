43f47508829f599be8d04a4d976bdb484433384f
Gallardo666 <jordigallardo0621@gmail.com>
2025-03-17 12:44:45 +0100

    Cambio de Color

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fiery Paws - Home</title>
+    <title>Fiery Paws</title>
     <link rel="icon" href="Images/Logos/red-panda.png">
     <link rel="stylesheet" href="styles.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
